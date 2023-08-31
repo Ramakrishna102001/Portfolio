b9527ba83cae1a711cd345ea984b72c7f56e3882
Ashwath <72975376+Ramakrishna102001@users.noreply.github.com>
2023-08-31 12:03:34 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+var Typed = new Typed(".multiple-text",{
+    strings: ["Java Developer","Frontend Developer","YouTuber"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true,
+})
